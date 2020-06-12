f0cc78942e9613ecab915306e12bf90be86800ac
ANgajasinghe <angajasinghe@gmail.com>
2020-06-12 05:31:38 +0530

    createServer const http = require('http'); const server = http.createServer(); server.listen(3800);

---
+++ Node.js-Basic/app.js
@@ -0,0 +1,28 @@
+//import packages
+/*
+    you can import your packages too : require(./relative-path ) :- .js not required in the end i will automaticaly added by node
+*/
+const http = require('http');
+
+// function requestListener(req,res){}
+// http.createServer(requestListener)
+
+//--- create a server using event driven architecture 
+
+// (ES5)
+// http.createServer(function(req,res){
+
+// });
+
+// (ES6)
+const server = http.createServer((req,res)=>{
+    console.log(req);
+});
+
+//keep this running and listen to the income req -> .listen()
+server.listen(3800);
+// to check -> http://localhost:3800/
+
+/*
+    Server was created -D
+*/
