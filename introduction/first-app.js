894c2fcd0260ae9c602a4e97984d6230547e77be
ANgajasinghe <angajasinghe@gmail.com>
2020-06-21 16:48:19 +0530

    WorkFlow : Install nodemon

--- introduction/first-app.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-//this culd not do in the browser ..
-fs.writeFileSync('newFileFromNode.txt','hello this is a data from node js file system');
-
-
-console.log('hello node js');
