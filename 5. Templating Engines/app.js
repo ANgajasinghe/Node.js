60acdab3f3742822d8e40a627287e0a9ff7d3cb0
ANgajasinghe <angajasinghe@gmail.com>
2021-09-20 15:11:43 +0530

    Completed TE

--- 5. Templating Engines/app.js
+++ 5. Templating Engines/app.js
@@ -1,22 +1,24 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+// setting template engines app.set('view engine', 'plug');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-
-// 404 error
 app.use((req, res, next) => {
-    res.status(404).send('<h1> Page not found </h1')
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- 5. Templating Engines/routes/admin.js
+++ 5. Templating Engines/routes/admin.js
@@ -2,28 +2,28 @@ const path = require('path');
 
 const express = require('express');
 
-const router = express.Router();
-
 const rootDir = require('../util/path');
+
+const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-
-  res.sendFile(path.join(rootDir, 'views','add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
-  
-  products.push({title:req.body.title})
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-
-// here we can export our whole module as well 
-//module.exports = router;
-
-// or export multiple values from this .js file.
 exports.routes = router;
 exports.products = products;
